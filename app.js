c44b37859cd9d5f8353fe7e9246cf0b2ab579b99
Talha836902 <talhaasiff786@gmail.com>
2023-10-08 02:19:58 -0700

    Files are added

---
+++ app.js
@@ -0,0 +1,52 @@
+// Prompt the user for a positive integer
+//const userInput = prompt("Enter a positive integer: ");
+//const number = parseInt(userInput);
+
+// Check if the input is a valid positive integer
+//if (!isNaN(number) && number > 0) {
+  // Calculate the round off value, floor value, and ceil value
+ // const roundOffValue = Math.round(number);
+//  const floorValue = Math.floor(number);
+ // const ceilValue = Math.ceil(number);
+
+  // Display the results
+  //console.log("Number: " + number);
+  //console.log("Round off value: " + roundOffValue);
+  //console.log("Floor value: " + floorValue);
+  //console.log("Ceil value: " + ceilValue);
+//} else {
+  //console.log("Invalid input. Please enter a positive integer.");
+//}
+
+
+
+// Prompt the user for a negative floating-point number
+//const number = parseFloat(prompt("Enter a negative floating-point number: "));
+
+// Check if the input is a valid number
+//if (!isNaN(number) && number < 0) {
+  // Calculate the round off value, floor value, and ceil value
+  //const roundOffValue = Math.round(number);
+  //const floorValue = Math.floor(number);
+  //const ceilValue = Math.ceil(number);
+
+  // Display the results
+  //console.log("Number: " + number);
+  //console.log("Round off value: " + roundOffValue);
+  //console.log("Floor value: " + floorValue);
+//   console.log("Ceil value: " + ceilValue);
+// } 
+// else {
+//   console.log("Invalid input. Please enter a negative floating-point number.");
+// }
+
+
+
+// Input your number here
+// const number = -4;
+
+// Calculate the absolute value
+//const absoluteValue = Math.abs(number);
+
+// Display the result
+//console.log(`The absolute value of ${number} is ${absoluteValue}`);
